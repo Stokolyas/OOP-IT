4a31b0c8a9dddae8719af3d5496a4b834f1a8d95
Unknown <alexey.stokolyas@gmail.com>
2018-07-31 11:58:42 +0300

    Agens3

--- Company.js
+++ Company.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-console,no-undef,max-statements,no-trailing-spaces */
 "use strict";
 // type == 1 web type = 2 mob
 class Task {
   constructor() {
     this.type = Math.floor(Math.random(2) * 2) + 1;
     this.hard = Math.floor(Math.random(3) * 3) + 1;
-    this.complete = 0;
+    this.complete;
 
     this.Complete = function(complete) {
       if (!arguments.length) return this.complete;
@@ -39,7 +40,7 @@ class Agent {
 // type == 1 web type == 2 mob type ==3 QA
 class Department {
   constructor() {
-    this.agents;
+    this.agents
     this.special;
 
     this.Agents = function(agent) {
@@ -57,74 +58,79 @@ class Department {
   }
 }
 
+var Tasks
+Tasks = []
+
 class Firm {
+
   constructor() {
-    this.Tasks = [];
-    this.tasks = 0;
+    this.countTask = 0;
   }
 
   gainTasks() {
-    this.tasks = Math.floor(Math.random(4) * 5);
-    console.log(this.tasks);
-    var taskLen = this.Tasks.length;
-    for (let index = taskLen; index < taskLen + this.tasks; index++) {
+    this.countTask = Math.floor(Math.random(4) * 5);
+    console.log(this.countTask);
+      let taskLen = Tasks.length;
+      for (var index = 0; index < this.countTask; index++) {
       let task = new Task();
       console.log(task);
-      this.Tasks[index] = task;
-      console.log(this.Tasks);
+      Tasks[index] = task;
+      console.log(Tasks);
     }
   }
 
   gainAgents() {
-    for (let index = 0; index < this.tasks; index++) {
+    console.log(this.countTask)
+    for (let index = 0; index < this.countTask; index++) {
       let agent = new Agent();
-      if (this.Tasks[index].Complete() == 1) {
-        departmentQA.Agents(agent);
-        agent.Practice(0);
-        agent.Task(this.Tasks[index]);
-        this.Tasks.splice(index, 1);
-        console.log(agent);
-      } else {
-        switch (this.Tasks[index].Type) {
-          case 1:
-            departmentWeb.Agents(agent);
-            agent.Practice(0);
-            console.log(this.Tasks[index]);
-            agent.Task(1);
-            this.Tasks.splice(index, 1);
-            console.log(agent);
-            break;
-          case 2:
-            departmentWeb.Agents(agent);
-            agent.Practice(0);
-            agent.Task(1);
-            this.Tasks.splice(index, 1);
-            console.log(agent);
-            break;
+      if (Tasks[index].Complete() != "undefined") {
+        if (Tasks[index].Complete() > 0) {
+          departmentQA.Agents(agent)
+          agent.Practice(0)
+          agent.Task(this.Tasks[index])
+          Tasks.splice(index, 1)
+          console.log(agent)
+        } else {
+          switch (Tasks[index].Type) {
+            case 1:
+              departmentWeb.Agents(agent)
+              agent.Practice(0)
+              agent.Task(1)
+              Tasks.splice(index, 1)
+              console.log(agent)
+              break
+            case 2:
+              departmentWeb.Agents(agent)
+              agent.Practice(0)
+              agent.Task(1)
+              Tasks.splice(index, 1)
+              console.log(agent)
+              break
+          }
         }
       }
     }
   }
   delAgent() {
     for (let index = 0; index < departmentMob.getAgents(); index++) {
-      if (departmentMob.Agents[index].practice == 4) {
+      if (departmentMob.Agents[index].practice === 4) {
         departmentMob.Agents.splice(index, 1);
       }
     }
     for (let index = 0; index < departmentWeb.getAgents(); index++) {
-      if (departmentWeb.Agents[index].practice == 4) {
+      if (departmentWeb.Agents[index].practice === 4) {
         departmentWeb.Agents.splice(index, 1);
       }
     }
     for (let index = 0; index < departmentQA.getAgents(); index++) {
-      if (departmentQA.Agents[index].practice == 4) {
+      if (departmentQA.Agents[index].practice === 4) {
         departmentQA.Agents.splice(index, 1);
       }
     }
   }
 
   Work() {
-    for (let index = 0; index < Tasks.length; index++) {
+    for (let index = 0; index < this.Tasks.length; index++) {
       if (Tasks[index].complete != 1) {
         Agents.forEach(function(item) {
           if (
@@ -162,9 +168,3 @@ let firm = new Firm();
 firm.gainTasks(); // получение тасков 1 день
 firm.gainAgents();
 
-function day() {
-  firm.gainTasks(); //получение тасков
-  firm.gainAgents(); //найм работнков
-  //передача проектов
-  firm.delAgent(); // увольнение
-}
